e21282e631d662495b70867402fcd5800cdc18ad
amdnet <machmudseo@gmail.com>
2021-10-02 23:12:40 +0700

    user

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -194,7 +194,7 @@
             <a href="#" class="nav-link">
               <i class="nav-icon fas user-circle"></i>
               <p>
-                User
+                Users
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
