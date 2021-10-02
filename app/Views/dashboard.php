27f7f04036907ab4f6f280f1c3a51326655637df
amdnet <machmudseo@gmail.com>
2021-10-02 23:24:34 +0700

    dashboard user

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -192,9 +192,8 @@
 
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas user-circle"></i>
-              <p>
-                Users
+              <i class="nav-icon fas fa-user-circle"></i>
+              <p> User
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
