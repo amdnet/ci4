89edbca67656b854bccacc5be1517e28889bbf4c
amdnet <machmudseo@gmail.com>
2021-10-02 22:33:55 +0700

    some init msg

---
+++ app/Views/layout_login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in (v2)</title>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>/assets/css/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>/assets/css/adminlte.min.css">
+</head>
+ 	
+<?= $this->renderSection('content') ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>/assets/js/adminlte.min.js"></script>
+</body>
+</html>                
